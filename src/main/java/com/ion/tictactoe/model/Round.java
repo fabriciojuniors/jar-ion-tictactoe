ee72814cd9dc2cc916a3cf9d33ea64dae1e7c36d
Junior Souza <fabriciojuniorsc@gmail.com>
2021-09-11 18:10:11 -0300

    Ajuste tabuleiro

--- src/main/java/com/ion/tictactoe/model/Round.java
+++ src/main/java/com/ion/tictactoe/model/Round.java
@@ -116,42 +116,42 @@ public class Round implements Serializable {
 
     public boolean setWinner() {
         for(int i = 0; i < 3; i++){
-            if(!this.board[i][0].equals("") &&this.board[i][0].equals(this.board[i][1]) && this.board[i][0].equals(this.board[i][2])){
+            if(!this.board[i][0].equals("-") &&this.board[i][0].equals(this.board[i][1]) && this.board[i][0].equals(this.board[i][2])){
                 this.winner = this.board[i][0].equals("x") ? this.player1 : this.player2;
                 return true;
             }
         }
 
         if(this.board[0][0].equals(this.board[1][1]) && this.board[0][0].equals(this.board[2][2])){
-            if(!this.board[0][0].equals("")){
+            if(!this.board[0][0].equals("-")){
                 this.winner = this.board[0][0].equals("x") ? this.player1 : this.player2;
                 return true;
             }
         }
 
         if(this.board[0][2].equals(this.board[1][1]) && this.board[0][0].equals(this.board[2][0])){
-            if(!this.board[0][2].equals("")){
+            if(!this.board[0][2].equals("-")){
                 this.winner = this.board[0][0].equals("x") ? this.player1 : this.player2;
                 return true;
             }
         }
 
         if(this.board[0][0].equals(this.board[1][0]) && this.board[0][0].equals(this.board[2][0])){
-            if(!this.board[0][0].equals("")){
+            if(!this.board[0][0].equals("-")){
                 this.winner = this.board[0][0].equals("x") ? this.player1 : this.player2;
                 return true;
             }
         }
 
         if(this.board[0][1].equals(this.board[1][1]) && this.board[0][1].equals(this.board[2][1])){
-            if(!this.board[0][1].equals("")){
+            if(!this.board[0][1].equals("-")){
                 this.winner = this.board[0][1].equals("x") ? this.player1 : this.player2;
                 return true;
             }
         }
 
         if(this.board[0][2].equals(this.board[1][2]) && this.board[0][2].equals(this.board[2][2])){
-            if(!this.board[0][2].equals("")){
+            if(!this.board[0][2].equals("-")){
                 this.winner = this.board[0][2].equals("x") ? this.player1 : this.player2;
                 return true;
             }
@@ -165,7 +165,7 @@ public class Round implements Serializable {
         Integer coluna = new Random().nextInt(3);
         Logger.getLogger("BotPlay").info("Linha: " + linha + "\tColuna: " + coluna);
 
-        if(this.board[linha][coluna].equals("")){
+        if(this.board[linha][coluna].equals("-")){
             this.board[linha][coluna] = "o";
             return;
         }
@@ -177,7 +177,7 @@ public class Round implements Serializable {
 
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
-                board[i][j] = "";
+                board[i][j] = "-";
             }
         }
         this.board = board;
@@ -192,7 +192,7 @@ public class Round implements Serializable {
 
         for(int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (this.board[i][j].equals("")) {
+                if (this.board[i][j].equals("-")) {
                     emBrancos++;
                 }
             }
