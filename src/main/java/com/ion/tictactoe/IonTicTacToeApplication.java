0abb41c876a067c50dbaaef6eb1beaf8c62b54ee
Junior Souza <fabriciojuniorsc@gmail.com>
2021-09-11 16:27:50 -0300

    Ajuste de dependencia

--- src/main/java/com/ion/tictactoe/IonTicTacToeApplication.java
+++ src/main/java/com/ion/tictactoe/IonTicTacToeApplication.java
@@ -13,25 +13,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@Configuration
 public class IonTicTacToeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IonTicTacToeApplication.class, args);
-	}
-
-	@Bean
-	public FilterRegistrationBean corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
-		config.addAllowedMethod(HttpMethod.PUT);
-		config.addAllowedMethod(HttpMethod.DELETE);
-		config.addAllowedMethod(HttpMethod.GET);
-		config.addAllowedMethod(HttpMethod.POST);
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-		bean.setOrder(0);
-		return bean;
 	}
 
 }
